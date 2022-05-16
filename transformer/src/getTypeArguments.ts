40933808d79975d5b11e9dca232d849afdb303f8
Joe Ferner <joe@fernsroth.com>
2022-05-16 21:09:43 +0000

    wip generics

--- transformer/src/getTypeArguments.ts
+++ transformer/src/getTypeArguments.ts
@@ -3,15 +3,16 @@ import { Context }                   from "./contexts/Context";
 import { GetTypeCall }               from "./declarations";
 import { getTypeCall }               from "./getTypeCall";
 
-export function getTypeArguments(node: ts.Node | ts.Type | ts.Symbol, context: Context): GetTypeCall[] | undefined
+export function getTypeArguments(t: ts.Node | ts.Type | ts.Symbol, context: Context): GetTypeCall[] | undefined
 {
-    if ((node as ts.Symbol).declarations?.[0]) {
-        const declaration = (node as ts.Symbol).declarations?.[0];
-        if ((declaration as any)?.type) {
-            node = (declaration as any)?.type;
-        }
+    // if we have a symbol get the type from the declaration if available
+    const declaration = (t as ts.Symbol).declarations?.[0];
+    const declarationType = (declaration as ts.SignatureDeclarationBase)?.type;
+    if (declarationType) {
+        t = declarationType;
     }
-	const typeArgs: ts.Node[] | ts.Type[] | undefined = (node as any).resolvedTypeArguments ?? (node as any).typeArguments;
+
+    const typeArgs: ts.Node[] | ts.Type[] | undefined = (t as any).resolvedTypeArguments ?? (t as ts.NodeWithTypeArguments).typeArguments;
 	return typeArgs?.map(t => {
         const isNode = (t as any).parent;
         return getTypeCall(
